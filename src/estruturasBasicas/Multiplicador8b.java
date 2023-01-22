8eb9ba5a2b338d4c0794d9ebb000307b6d1838ce
pedroanto <95453438+pedroanto@users.noreply.github.com>
2023-01-22 19:25:44 -0300

    Update Multiplicador8b.java

--- src/estruturasBasicas/Multiplicador8b.java
+++ src/estruturasBasicas/Multiplicador8b.java
@@ -4,17 +4,14 @@ public class Multiplicador8b{
   /// acho que é assim que fica a multiplicação faltou só a questão do estouro 
 public void executar(boolean[] S1, boolean[] S2, boolean Cin){   
 Conversor converter = new Conversor(); 
+boolean value_char[] = new boolean [8];
 if(Cin == true){ 
-try {
+
           int foo= converter.paraInteiro(S1);
           int foo2 = converter.paraInteiro(S1);
             int result = foo *foo2;
-            boolean value_char=paraVetBoolean(result);
-             //System.out.println(value_char);
-           
-        } catch (NumberFormatException e) {
-            System.out.println("Error: The binary string is not valid");
-        }
+            value_char[]=paraVetBoolean(result);
+          
 }
       
     }
