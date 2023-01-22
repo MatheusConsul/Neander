49bbd2bb148b1b2cb3943d9cc118d26e33cca71d
pedroanto <95453438+pedroanto@users.noreply.github.com>
2023-01-22 19:13:07 -0300

    Update UnidadeControle.java

--- src/elementosDaArquitetura/UnidadeControle.java
+++ src/elementosDaArquitetura/UnidadeControle.java
@@ -3,7 +3,36 @@ package elementosDaArquitetura;
 
 public class UnidadeControle {
 
-    
+   /// menu da unidade de controle
+   public void executar(boolean vetResultante[]){
+    if(vetResultante[0] == false && vetResultante[1] == false && vetResultante[2] == false && vetResultante[3] == false){
+      // 0000 menu que seleciona no decod a operação que vai ser feita 
+    }
+    if(vetResultante[0] == false && vetResultante[1] == false && vetResultante[2] == false && vetResultante[3] == true){
+      // 0001 menu que seleciona no decod a operação que vai ser feita 
+    }
+    if(vetResultante[0] == false && vetResultante[1] == false && vetResultante[2] == true && vetResultante[3] == false){
+      //0010 menu que seleciona no decod a operação que vai ser feita 
+    }
+    if(vetResultante[0] == false && vetResultante[1] == false && vetResultante[2] == true && vetResultante[3] == true){
+      //0011 menu que seleciona no decod a operação que vai ser feita 
+    }
+    if(vetResultante[0] == false && vetResultante[1] == true && vetResultante[2] == false && vetResultante[3] == false){
+      //0100  menu que seleciona no decod a operação que vai ser feita 
+    }
+    if(vetResultante[0] == false && vetResultante[1] == true && vetResultante[2] == false && vetResultante[3] == true){
+      //0101  menu que seleciona no decod a operação que vai ser feita 
+    }
+    if(vetResultante[0] == false && vetResultante[1] == true && vetResultante[2] == true && vetResultante[3] == false){
+      //0110  menu que seleciona no decod a operação que vai ser feita 
+    }
+    if(vetResultante[0] == false && vetResultante[1] == true && vetResultante[2] == true && vetResultante[3] == true){
+      //0111  menu que seleciona no decod a operação que vai ser feita 
+    }
+    if(vetResultante[0] == true && vetResultante[1] == false && vetResultante[2] == false && vetResultante[3] == false){
+      // 1000 menu que seleciona no decod a operação que vai ser feita 
+    }
+  } 
 
     
 }
